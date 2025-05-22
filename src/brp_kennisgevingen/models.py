5e72ce3f299e9174d123537b1f2ce38afefa193b
Jasper Jager <j.jager@amsterdam.nl>
2025-05-22 09:03:49 +0200

    Add audit date time fields to subscription model

--- src/brp_kennisgevingen/models.py
+++ src/brp_kennisgevingen/models.py
@@ -47,6 +47,8 @@ class Subscription(models.Model):
     bsn = models.CharField(max_length=9)
     start_date = models.DateField()
     end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = SubscriptionManager()
 
