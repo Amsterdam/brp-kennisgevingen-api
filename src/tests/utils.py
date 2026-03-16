30c9efbd68ebed0f9867cc20e8ac44fac3cc3e8b
Pieter Stokkink <p.stokkink@amsterdam.nl>
2026-03-16 11:42:13 +0100

    fix tests with JWKSWrapper

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,7 +1,7 @@
 import time
 from datetime import date
 
-from authorization_django import jwks
+from authorization_django.jwks import JWKSWrapper
 from jwcrypto.jwt import JWT
 from rest_framework.renderers import JSONRenderer
 from rest_framework.request import Request
@@ -34,7 +34,8 @@ def build_jwt_token(scopes, subject="test@example.com", appid="application_id"):
     now = int(time.time())
 
     kid = "2aedafba-8170-4064-b704-ce92b7c89cc6"
-    key = jwks.get_keyset().get_key(kid)
+    jwks = JWKSWrapper()
+    key = jwks.keyset.get_key(kid)
     token = JWT(
         header={"alg": "ES256", "kid": kid},
         claims={"iat": now, "exp": now + 30, "scopes": scopes, "sub": subject, "appid": appid},
